77178a7ed5a3734aa865f2be7667eb4695fd6cd5
lysbrodya <lysbrodya@gmail.com>
2024-03-19 17:33:42 +0200

    compon

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
-import App from "./Components/App/App";
+import App from "./components/App/App";
 import "./index.css";
 
 import { store, persistor } from "./redux/store";
